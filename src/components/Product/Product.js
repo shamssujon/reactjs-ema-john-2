6cc6f978685d35b5b252630da54862161e691a85
Shamsul Islam Sujon <sujon2762@gmail.com>
2022-10-04 06:16:02 +0600

    Display product data

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,21 +1,53 @@
 import React from "react";
 
-const Product = () => {
+const Product = ({ product }) => {
+    const { category, img, name, price, seller, shipping } = product;
     return (
-        <div className="border bg-white shadow rounded-lg overflow-hidden">
+        <div className="group flex flex-col justify-between overflow-hidden rounded-lg border bg-white shadow transition hover:shadow-lg">
             <div className="aspect-4/3 overflow-hidden ">
                 <img
-                    src="https://loremflickr.com/320/240"
+                    src={img}
                     alt=""
-                    className="h-full w-full object-cover"
+                    className="scale-1 h-full w-full transform object-cover transition group-hover:scale-[1.02]"
                 />
             </div>
-            <div className="p-4">
-                <h5 className="text-2xl">Product name </h5>
-                <h6 className="text-xl">Price</h6>
+            <div className="flex-1 p-4">
+                <p className="mb-2 text-sm text-slate-600">
+                    <a href="/">{category}</a>
+                </p>
+                <h6 className="mb-2 text-xl font-semibold capitalize transition hover:text-orange-600">
+                    <a href="/" className="block">
+                        {name.length > 30
+                            ? name.slice(0, 30).toLowerCase() + "..."
+                            : name.toLowerCase()}
+                    </a>
+                </h6>
+                <p className="text-lg">
+                    <span className="text-slate-600">Price:</span>{" "}
+                    <span className="font-semibold text-orange-600">${price}</span>
+                </p>
+                <p className="text-sm">
+                    <span className="text-slate-600">+ Shipping:</span>{" "}
+                    <span className="font-semibold">${shipping}</span>
+                </p>
             </div>
             <div className="pt-2">
-                <button className="bg-orange-600 text-white px-6 py-3 w-full">Add to cart</button>
+                <button className="flex w-full items-center justify-center gap-2 bg-orange-600 px-6 py-3 text-center font-semibold uppercase tracking-wide text-white">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="h-6 w-6">
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                        />
+                    </svg>
+                    <span>Add to cart</span>
+                </button>
             </div>
         </div>
     );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch("products.json")
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    }, []);
     return (
         <div className="col-span-3">
-            <h3 className="mb-8 text-4xl font-semibold">Latest Products</h3>
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                <Product></Product>
+            <h3 className="mb-8 text-4xl font-semibold">Latest Products {products.length}</h3>
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+                {products.map((product) => console.log(product))}
+                {products.map((product) => (
+                    <Product key={product.id} product={product}></Product>
+                ))}
             </div>
         </div>
     );
